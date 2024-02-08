184d13a139fcaa93a89adf0444d4cc1a95b94790
Elias Larsen <s235125@dtu.dk>
2024-02-08 11:11:49 +0100

    ADDED: Write to file

--- checkForUpload.py
+++ checkForUpload.py
@@ -1,11 +1,14 @@
 import requests
-  
 # Making a GET request
-r = requests.get('https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCIoxBbxCTaoYcwsSowTIsVA&maxResults=100&order=date&type=video&key=AIzaSyCLF1ucUAwxmBXvsFsyh_wEJAXfSBN8iJQ')
+r = requests.get('https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCIoxBbxCTaoYcwsSowTIsVA&maxResults=5&order=date&type=video&key=AIzaSyCLF1ucUAwxmBXvsFsyh_wEJAXfSBN8iJQ')
  
 # check status code for response received
 # success code - 200
 print(r)
  
 # print content of request
-print(r.text)
+print(type(r.text))
+
+file1 = open("videos.txt", "w")
+file1.write(str(r.text))
+file1.close() 
