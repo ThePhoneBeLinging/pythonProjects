c213d092b010358ebf77d95fd7ad94a779ba8635
Elias Larsen <s235125@dtu.dk>
2024-02-10 12:17:55 +0100

    Now get every line of file independently

--- checkForUpload.py
+++ checkForUpload.py
@@ -1,7 +1,6 @@
 import requests
 # Making a GET request
 r = requests.get('https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCIoxBbxCTaoYcwsSowTIsVA&maxResults=5&order=date&type=video&key=AIzaSyCLF1ucUAwxmBXvsFsyh_wEJAXfSBN8iJQ')
- 
 # check status code for response received
 # success code - 200
 print(r)
